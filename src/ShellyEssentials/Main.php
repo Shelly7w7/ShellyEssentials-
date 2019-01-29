5989dfbb660e022d8dd6ac4000a263cc6236c9b2
piyushbest <42518831+piyushbest@users.noreply.github.com>
2019-01-29 11:45:33 +0530

    For The Friend Server

--- src/ShellyEssentials/Main.php
+++ src/ShellyEssentials/Main.php
@@ -31,7 +31,7 @@ use ShellyEssentials\tasks\ClearLaggTask;
 
 class Main extends PluginBase{
 
-	public const PREFIX = TextFormat::RED . TextFormat::BOLD . "ObliviousCraft > " . TextFormat::RESET;
+	public const PREFIX = TextFormat::RED . TextFormat::BOLD . "PixelCraftMC > " . TextFormat::RESET;
 
 	public function onEnable() : void{
 		API::$instance = $this;
